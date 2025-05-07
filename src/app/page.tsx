0c9afbf9e366ed99dff7e4f1c7801982654d45d9
kolesnykalexey <kolesnykalexey23@gmail.com>
2025-05-07 12:51:49 +0300

    test feature for ci/cd in new branch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 export default function Page() {
   return (
       <div>
-        <h1>Home</h1>
+        <h1>Home sweet home</h1>
         <Link href="/about">About</Link>
       </div>
   );
